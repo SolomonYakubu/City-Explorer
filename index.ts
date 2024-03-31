77c2420b149e6d42e4b3b75b14a10b509bda0491
dev <yunusahabibat8@gmail.com>
2024-03-31 09:34:52 +0100

    changes

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi there! I am city explorer, how may i help you');
-});
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hi there! I am city explorer, how may i help you');
+// });
+
+app.use("/", require("./model.ts"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
