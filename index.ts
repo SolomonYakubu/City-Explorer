c63dc5dd838f947924ff946d2b121075bfc0be30
dev <yunusahabibat8@gmail.com>
2024-04-01 09:59:48 +0100

    changes

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from "express";
 import runChat from "./model";
+import path from 'path';
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hi there! I am city explorer, how may i help you");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hi there! I am city explorer, how may i help you");
+// });
 app.post("/message", async (req: Request, res, Response) => {
   const message = req.body.message;
   const reply = await runChat(message);
@@ -16,6 +17,11 @@ app.post("/message", async (req: Request, res, Response) => {
 });
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
